4a660485777fa47f69fc70d764d66ec20a799e37
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2025-10-02 22:21:29 -0300

    Add Beecrowd1000

---
+++ beecrowd1000/Beecrowd1000.java
@@ -0,0 +1,7 @@
+public class Beecrowd1000 {
+    //hello world
+    //print the message Hello World!
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

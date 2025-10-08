a12d26eb73b932eb8880f71202f8e9c521e47372
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2025-10-07 21:28:20 -0300

    Add Beecrowd1001

---
+++ beecrowd1001/Beecrowd1001.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Beecrowd1001 {
+    
+    //extremely basic
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int X = A + B;
+
+        System.out.println("X = " + X + "\n");
+
+        scanner.close();
+    }
+}
+

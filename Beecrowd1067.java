45a60ffa8f60f39b73be1cbdd4f8501699ba8d8e
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2025-11-02 22:20:51 -0300

    Create Beecrowd1067

---
+++ Beecrowd1067.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Beecrowd1067 {
+    public static void main(String[] args) {
+
+        //odd numbers
+        int x;
+        Scanner scanner = new Scanner(System.in);
+        x = scanner.nextInt();
+
+        for (int i = 1; i <= x; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+        scanner.close();
+    }
+}

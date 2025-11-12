c9f28b650a6417568b3da7b255c9db5968ee9524
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2025-11-12 15:00:00 -0300

    Update Beecrowd 1051

---
+++ beecrowd1051/Beecrowd1051.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Beecrowd1051again {
+
+    public static void main(String[] args){
+
+        // taxes
+
+        Scanner scanner = new Scanner(System.in);
+
+        double salary = scanner.nextDouble();
+        double taxes;
+
+        if(salary <= 2000){
+            System.out.print("Isento\n");
+        }
+        else if(salary <= 3000){
+            taxes = (salary - 2000) * 0.08;
+            System.out.printf("R$ %.2f\n", taxes);
+        }
+        else if(salary <= 4500){
+            taxes = (1000 * 0.08) + ((salary - 3000) * 0.18);
+            System.out.printf("R$ %.2f\n", taxes);
+        }
+        else{
+            taxes = (1000 * 0.08) + (1500 * 0.18) + ((salary - 4500) * 0.28);
+            System.out.printf("R$ %.2f\n", taxes);
+        }
+    }
+}

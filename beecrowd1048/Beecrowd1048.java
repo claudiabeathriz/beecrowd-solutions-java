f9d532c3b8cc568153e4b843b1078d64096e6c4b
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2025-12-03 12:44:49 -0300

    Create Beecrowd1048

---
+++ beecrowd1048/Beecrowd1048.java
@@ -0,0 +1,63 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Beecrowd1048 {
+
+    public static void main(String[] args){
+
+        // salary increase
+
+        Scanner scanner = new Scanner(System.in);
+
+        double salary = scanner.nextDouble();
+        DecimalFormat format = new DecimalFormat("#.##");
+        salary = Double.valueOf(format.format(salary));
+
+        double newSalary = calcNewSalary(salary);
+        double moneyEarned = calcMoneyEarned(salary, newSalary);
+        double percentage = calcPercentage(salary, newSalary);
+
+        System.out.printf("Novo salario: %.2f\n", newSalary);
+        System.out.printf("Reajuste ganho: %.2f\n", moneyEarned);
+        System.out.printf("Em percentual: %.0f %%\n", percentage);
+
+        scanner.close();
+    }
+
+    static double calcNewSalary(double salary){
+
+        double newSalary = 0;
+
+        if(salary <= 400){
+            newSalary = salary + (0.15 * salary);
+        } else if (salary >= 400.01 && salary <= 800.00) {
+            newSalary = salary + (0.12 * salary);
+        }
+        else if (salary >= 800.01 && salary <= 1200.00) {
+            newSalary = salary + (0.10 * salary);
+        }
+        else if (salary >= 1200.01 && salary <= 2000.00) {
+            newSalary = salary + (0.07 * salary);
+        }
+        else if (salary > 2000.01) {
+            newSalary = salary + (0.04 * salary);
+        }
+
+        return newSalary;
+    }
+
+    static double calcMoneyEarned(double salary, double newSalary){
+
+        double moneyEarned = newSalary - salary;
+
+        return moneyEarned;
+
+    }
+
+    static double calcPercentage(double salary, double newSalary){
+
+        double percentage = ((newSalary - salary) / salary) * 100;
+
+        return percentage;
+    }
+}

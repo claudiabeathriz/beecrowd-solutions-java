b9971b5e254abf083c6ec59cbc10bd6268f271b9
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-01-01 15:05:00 -0300

    Add solution to Beecrowd1074

--- beecrowd1074/Beecrowd1074.java
+++ beecrowd1074/Beecrowd1074.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
+
 public class Beecrowd1074 {
+
+    public static void main(String[] args){
+
+        // even or odd
+
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        for(int i = 0; i < N; i++){
+
+            int x = scanner.nextInt();
+
+            if(x == 0){
+                System.out.println("NULL");
+            }
+            else if(x > 0){
+                if(x % 2 == 0){
+                    System.out.println("EVEN POSITIVE");
+                }
+                if(x % 2 != 0){
+                    System.out.println("ODD POSITIVE");
+                }
+            }
+            else{
+                if(x % 2 == 0){
+                    System.out.println("EVEN NEGATIVE");
+                }
+                if(x % 2 != 0){
+                    System.out.println("ODD NEGATIVE");
+                }
+            }
+        }
+
+        scanner.close();
+    }
 }

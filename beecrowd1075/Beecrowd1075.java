a3bc741c9ad6a843e48b20f8da005f0305efcfbb
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-01-02 18:56:00 -0300

    Add solution to Beecrowd1075

--- beecrowd1075/Beecrowd1075.java
+++ beecrowd1075/Beecrowd1075.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Beecrowd1075 {
+
+    public static void main(String[] args){
+
+        // remaining 2
+
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        for(int i = 1; i < 10000; i++){
+            if(i % N == 2){
+                System.out.println(i);
+            }
+        }
+
+        scanner.close();
+    }
 }
